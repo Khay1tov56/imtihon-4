51e9851bd3e7969177a40a39f8c4d40db4f42ad9
NurilloOkeng5670 <khayitov5670@gmail.com>
2022-09-15 14:58:48 +0300

    Add carousel

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin:35,
+//     nav: true,
+//     autoplay: true,
+//     autoplayTimeout: 5000,
+//     stagePadding: 200,
+//     dots: false,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         768:{
+//             items:3
+//         },
+//         1000:{
+//             items:2
+//         }
+//     }
+// })
